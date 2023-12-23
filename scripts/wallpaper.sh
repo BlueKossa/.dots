af89427d2a4ac0b03d6f2e277768915350b78d9e
BlueKossa <dexter@stormdal.se>
2023-12-23 01:51:18 +0100

    Changed wp to be faster bcs laggy?

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sel=$(ls -1 $HOME/.dots/wallpapers | grep "\.jpg\|\.png" | rofi -dmenu -i -p "Select Wallpaper")
+sel=$(ls -1 $HOME/.dots/wallpapers | grep "\.jpg\|\.png\|\.gif" | rofi -dmenu -i -p "Select Wallpaper")
 
 if [ "$sel" ]; then
     echo $sel > $HOME/.cache/current_wallpaper
     wal -q -i $HOME/.dots/wallpapers/$sel
-    swww img $HOME/.dots/wallpapers/$sel
+    swww img $HOME/.dots/wallpapers/$sel --transition-step 50
     killall .waybar-wrapped
     killall waybar
     waybar &
